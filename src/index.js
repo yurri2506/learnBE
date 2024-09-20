e944724afc93d2b338c498757dd0d019bd4e7fde
yurri2506 <22520590@gm.uit.edu.vn>
2024-09-21 02:22:30 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config()
+const mongoose = require("mongoose");
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-  return res.send('Hello World');
-})
+app.get("/", (req, res) => {
+  return res.send("Hello World");
+});
+
+mongoose.connect(
+  `mongodb+srv://22520590:${process.env.MONGO_DB}@student.awha5.mongodb.net/`
+  )
+  .then(() => {
+    console.log("Connect database successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
-  console.log(`Server is running in port `, + port);
+  console.log(`Server is running in port `, +port);
 });
