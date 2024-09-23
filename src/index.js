4177fcc8e48ad860fe7b4bf99d9953a3f653439c
yurri2506 <22520590@gm.uit.edu.vn>
2024-09-23 20:10:11 +0700

    Phan chia MVC

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   return res.send("Hello World");
 });
 
+routes(app);
+
 mongoose.connect(
-  `mongodb+srv://22520590:${process.env.MONGO_DB}@student.awha5.mongodb.net/`
+  `${process.env.MONGO_DB}`
   )
   .then(() => {
     console.log("Connect database successfully");
